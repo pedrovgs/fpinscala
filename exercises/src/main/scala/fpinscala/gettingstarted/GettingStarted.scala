67d2db4ffef465d18f67dbd679ea3ca1db9ef545
pedrovgs <pedrovicente.gomez@gmail.com>
2016-08-22 22:31:02 +0200

    Add two more solutions to exercise 2.5

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -207,4 +207,12 @@ object PolymorphicFunctions {
     a => f(g(a))
   }
 
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    f compose g
+  }
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    g andThen f
+  }
+
 }
