d8ae151f446375d3c61a8c58fecf53bf1a40ea7a
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-08-29 16:06:46 +0200

    Add solution to exercise 3.23

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -138,4 +138,10 @@ object List { // `List` companion object. Contains functions for creating and wo
     case (Cons(y, ys), Cons(x, xs)) => Cons(y + x, sumPairs(ys, xs))
   }
 
+  def zipWith[A](a: List[A], b: List[A])(f: (A,A) => A): List[A] = (a,b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1,t1), Cons(h2,t2)) => Cons(f(h1,h2), zipWith(t1,t2)(f))
+  }
+
 }
