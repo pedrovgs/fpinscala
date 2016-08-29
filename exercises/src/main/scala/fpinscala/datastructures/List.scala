ffd04bb4f2c0e872b2c8feb65e5b5af0540c7378
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-08-29 16:20:23 +0200

    Add solution to exercise 3.24

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -144,4 +144,19 @@ object List { // `List` companion object. Contains functions for creating and wo
     case (Cons(h1,t1), Cons(h2,t2)) => Cons(f(h1,h2), zipWith(t1,t2)(f))
   }
 
+  @tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l, prefix) match {
+    case (_,Nil) => true
+    case (Cons(h, t),Cons(h2, t2)) if h == h2 => startsWith(t, t2)
+    case _ => false
+  }
+
+  @tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => sub == Nil
+    case _ if startsWith(sup, sub) => true
+    case Cons(_,t) => hasSubsequence(t, sub)
+  }
+
+
 }
