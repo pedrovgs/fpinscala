d90645f02a0bf5c19791b4e536fe01d608e66bad
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-08-29 15:32:16 +0200

    Add solution to exercise 3.17

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -113,5 +113,7 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def appendOne(l: List[Int]): List[Int] = foldRight(l, List[Int]())((x, acc) => Cons(x + 1, acc))
 
+  def toListString(l: List[Double]): List[String] = foldRight(l, List[String]())((x, acc) => Cons( x.toString, acc))
+
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
