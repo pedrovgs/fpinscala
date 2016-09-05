2097d39cca196020038bc63665ddcb337254112b
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-09-05 21:56:28 +0200

    Resolve exercise 10.16

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -169,7 +169,11 @@ object Monoid {
   }
 
   def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
-    sys.error("todo")
+    new Monoid[(A, B)] {
+      def op(x: (A, B), y: (A, B)) =
+        (A.op(x._1, y._1), B.op(x._2, y._2))
+      val zero = (A.zero, B.zero)
+    }
 
   def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] =
     sys.error("todo")
