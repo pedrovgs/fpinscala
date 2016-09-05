049e125216f496efd3a645a1c381e7b0bcafa3b1
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-09-05 20:31:36 +0200

    Resolve exercise 10.1

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -21,13 +21,29 @@ object Monoid {
     val zero = Nil
   }
 
-  val intAddition: Monoid[Int] = sys.error("todo")
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
 
-  val intMultiplication: Monoid[Int] = sys.error("todo")
+    override def zero: Int = 0
+  }
 
-  val booleanOr: Monoid[Boolean] = sys.error("todo")
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
 
-  val booleanAnd: Monoid[Boolean] = sys.error("todo")
+    override def zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override def zero: Boolean = true
+  }
 
   def optionMonoid[A]: Monoid[Option[A]] = sys.error("todo")
 
