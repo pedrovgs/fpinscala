03293f61d485ff9a0944f6579f43b8256c2472c9
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-09-06 11:59:59 +0200

    Resolve exercise 11.17

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -118,7 +118,17 @@ object Monad {
       st flatMap f
   }
 
-  val idMonad: Monad[Id] = ???
+  case class Id[A](value: A) {
+    def map[B](f: A => B): Id[B] = Id(f(value))
+    def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+  }
+
+  object Id {
+    val idMonad = new Monad[Id] {
+      def unit[A](a: => A) = Id(a)
+      def flatMap[A,B](ida: Id[A])(f: A => Id[B]): Id[B] = ida flatMap f
+    }
+  }
 
   def readerMonad[R] = ???
 }
